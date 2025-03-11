14044572c7ad5975d294e448091eaed60e3ba30b
SamuelFN123 <samuelfraguas123@gmail.com>
2025-03-11 13:09:39 +0100

    feat(main.py): Añadir main al repositorio

---
+++ src/main.py
@@ -0,0 +1,18 @@
+sets = {
+    "k-one": ["ID", "NV", "UT"],
+    "k-two": ["WA", "ID", "MT"],
+    "k-three": ["OR", "NV", "CA"],
+    "k-four": ["NV", "UT"],
+    "k-five": ["CA", "AZ"],
+    "k-six": ["NM", "TX", "OK"],
+    "k-seven": ["OK", "KS", "CO"],
+    "k-eight": ["KS", "CO", "NE"],
+    "k-nine": ["NE", "SD", "WY"],
+    "k-ten": ["ND", "IA"],
+    "k-eleven": ["MN", "MO", "AR"],
+    "k-twelve": ["LA"],
+    "k-thirteen": ["MO", "AR"],
+}
+
+if __name__ == "__main__":
+    pass
