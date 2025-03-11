ad5ce9e6d2cf6987a4931e0364366f8ead219610
SamuelFN123 <samuelfraguas123@gmail.com>
2025-03-11 13:19:39 +0100

    feat(main.py): Encapsular sets en una clase

--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
-sets = {
-    "k-one": ["ID", "NV", "UT"],
-    "k-two": ["WA", "ID", "MT"],
-    "k-three": ["OR", "NV", "CA"],
-    "k-four": ["NV", "UT"],
-    "k-five": ["CA", "AZ"],
-    "k-six": ["NM", "TX", "OK"],
-    "k-seven": ["OK", "KS", "CO"],
-    "k-eight": ["KS", "CO", "NE"],
-    "k-nine": ["NE", "SD", "WY"],
-    "k-ten": ["ND", "IA"],
-    "k-eleven": ["MN", "MO", "AR"],
-    "k-twelve": ["LA"],
-    "k-thirteen": ["MO", "AR"],
-}
+class Estation:
+
+    def __init__(self):
+        
+        self.sets = {
+            "k-one": {"ID", "NV", "UT"},
+            "k-two": {"WA", "ID", "MT"},
+            "k-three": {"OR", "NV", "CA"},
+            "k-four": {"NV", "UT"},
+            "k-five": {"CA", "AZ"},
+            "k-six": {"NM", "TX", "OK"},
+            "k-seven": {"OK", "KS", "CO"},
+            "k-eight": {"KS", "CO", "NE"},
+            "k-nine": {"NE", "SD", "WY"},
+            "k-ten": {"ND", "IA"},
+            "k-eleven": {"MN", "MO", "AR"},
+            "k-twelve": {"LA"},
+            "k-thirteen": {"MO", "AR"},
+        }
+
+    def get_estation(self, estation):
+        return self.sets[estation]
 
 if __name__ == "__main__":
     pass
