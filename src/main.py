058a4e4bb412408ca8ace5bba01ab984de53fe09
SamuelFN123 <samuelfraguas123@gmail.com>
2025-03-11 13:38:50 +0100

    feat(main.py): Añadir clase estacion

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 class Estation_Manager:
 
     def __init__(self):
-        
-        self.sets = {
+        sets = {
             "k-one": {"ID", "NV", "UT"},
             "k-two": {"WA", "ID", "MT"},
             "k-three": {"OR", "NV", "CA"},
@@ -17,13 +16,18 @@ class Estation_Manager:
             "k-twelve": {"LA"},
             "k-thirteen": {"MO", "AR"},
         }
+        self.estations = [Estation(name, countries) for name, countries in sets.items()]
 
-    def get_estation(self, estation):
-        return self.sets[estation]
-    
+
+    def __getitem__(self, name):
+        for estation in self.estations:
+            if estation.name == name:
+                return estation
+
     def get_most_covered_estation(self):
         pass
-        
+
+
     def sort(self):
         sets = [item for item in self.sets.values()]
         stop = len(sets)
@@ -32,6 +36,14 @@ class Estation_Manager:
                 if len(sets[j]) < len(sets[j+1]):
                     sets[j], sets[j+1] = sets[j+1], sets[j]
         return sets
+
+
+
+class Estation:
+    def __init__(self, name, countries):
+        self.name = name
+        self.countries = countries
+
 
 if __name__ == "__main__":
     pass
